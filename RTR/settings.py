8fcab89a78eae842d426eb6ef575dabaf769471d
cuboalcub <fvera8192@gmail.com>
2025-10-15 13:37:14 -0600

    Update ALLOWED_HOSTS in settings.py to include production and local hosts

--- RTR/settings.py
+++ RTR/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.getenv("SECRET_KEY", "refaccionaria_secreta")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        'refaccionariaback.onrender.com',
+        'localhost'
+]
 
 
 # Application definition
