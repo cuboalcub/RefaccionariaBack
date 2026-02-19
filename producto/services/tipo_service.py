981e3a698971bc7a81d2aafef1140233e5cc5dac
cuboalcub <fvera8192@gmail.com>
2026-02-19 11:39:43 -0600

    borre la venv que subiste y mejore con IA el html (solo estilos)

--- producto/services/tipo_service.py
+++ producto/services/tipo_service.py
@@ -1,16 +1,13 @@
-from producto.models import Tipo    
+from producto.models import Tipo
 from producto.repositories.tipo_repository import TipoRepository
-from usuario.repositories.usuario_repositorie import UserRepository     
 from repository.base_service import BaseService
+
 
 class TipoService(BaseService):
     def __init__(self):
         super().__init__(model=Tipo, repository=TipoRepository())
-    
+
     # Puedes sobrescribir métodos si necesitas lógica específica
     def _to_dict(self, instance):
         """Ejemplo: Personalizar cómo se serializa Tipo"""
-        return {
-            "id": instance.id,
-            "nombre": instance.nombre
-        }
+        return {"id": instance.id, "nombre": instance.nombre}
